573730e2ad49909883f780d18ec7d20a38f01d87
Thomas <t.collet974@outlook.fr>
2023-09-23 10:40:59 +0200

    v1.3

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import searcher
 import sys
 import re
 import time
+import json
+import requests
 
 def get_words(args) :
     try :
@@ -37,23 +39,33 @@ def print_found(found) :
         print("\t " + headers)
     return retour
 
-def print_retour_directory(retour):
+def print_retour_directory(args, retour):
     print("Output of " + retour["title"])
     print("GREEN CODE :")
     for key, value in retour["url"].items():
         if value["code"] < 400 :
             print("[" + visual.green(str(value["code"])) + "] " + key)
-    print("Forbidden Code :")
+            if args.find is not None :
+                if value["find"]["body"] is not None :
+                    for found in value["find"]["body"] :
+                        print("\t " + visual.blue("Found : ") + found)
+    print("\n")
+    print("RED CODE :")
     for key, value in retour["url"].items():
-        if value["code"] == 403 :
+        if value["code"] >= 400 :
             print("[" + visual.red(str(value["code"])) + "] " + key)
+    print("\n")
 
 def prepare_url(origin, suffix) :
     if suffix.startswith(origin) :
         new_url = suffix
-    elif len(suffix) == 0 or len(suffix) > 0 and (suffix[0] == "/" or suffix[-1] == "/")  :
+    elif suffix.startswith("https://") or suffix.startswith("http://") :
+        new_url = suffix
+    elif len(suffix) == 0 or (len(suffix) > 0 and (suffix[0] == "/" or origin[-1] == "/"))  :
         if origin.endswith("index.php") :
             origin = origin[:-9]
+        if len(suffix) > 0 and suffix[0] == "/" and origin[-1] == "/" :  
+            suffix = suffix[1:]
         new_url = origin + suffix  
     else :
         new_url = origin + "/" + suffix
@@ -67,7 +79,7 @@ def send_url(args, origin_url, words) :
     retour["ok"] = {}
     retour["sent"] = []
     current_url = origin_url
-    print("Fuzzing on " + origin_url)
+    # print("Fuzzing on " + origin_url)
     for word in words :
         if args.slow is not None :
             time.sleep(float(args.slow))
@@ -80,10 +92,11 @@ def send_url(args, origin_url, words) :
                 print("[" + visual.red(str(response.status_code)) + "] " + current_url)
             else :
                 print("[" + visual.green(str(response.status_code)) + "] " + current_url)
-                retour["ok"][current_url] = response
         else :
             if nb_request % 100 == 0 :
                 print(str(nb_request) + " requests sent")
+        if response.status_code < 400 : 
+            retour["ok"][current_url] = response
         retour["url"][current_url] = {} 
         retour["url"][current_url]["code"] = response.status_code
         if args.find is not None :
@@ -91,31 +104,37 @@ def send_url(args, origin_url, words) :
             retour["url"][current_url]["find"] = print_found(found)
         else :
             retour["url"][current_url]["find"] = {}
-    print(str(nb_request) + " requests sent")
+    # print(str(nb_request) + " requests sent")
     return {"retour":retour, "nb_request":nb_request}
 
-def intelligent(args, urls, sent_urls, words) :
-    stack_urls = urls
-    print("+ STARTING INTELLIGENT SCAN OF RESPONSE")
+def intelligent(args, sent_urls, words) :
+    stack_urls = []
+    if not args.silent : 
+        print("+ STARTING INTELLIGENT SCAN OF RESPONSE")
     retour = {}
     retour["ok"] = {}
     retour["url"] = {}
+    current_retour = send_url(args, args.url, [""])["retour"]
+    retour["ok"].update(current_retour["ok"])
+    retour["url"].update(current_retour["url"])
+    stack_urls.append(current_retour["ok"])
+    sent_urls.append(args.url)
     while stack_urls :
-        url, response = stack_urls.popitem()
-        new_urls = searcher.search_url(response.text)
+        response = stack_urls.pop()
+        new_urls = searcher.search_url(response[next(iter(response))].text)
         if new_urls :
-            for new_url in new_urls :
-                new_url = prepare_url(args.url, new_url)
+            for new_suffix in new_urls :
+                new_url = prepare_url(args.url, new_suffix)
                 if new_url not in sent_urls :
-                    current_retour = send_url(args, new_url, words)["retour"]
+                    current_retour = send_url(args, new_url, [""] + words)["retour"]
                     retour["ok"].update(current_retour["ok"])
                     retour["url"].update(current_retour["url"])
-                    stack_urls.update(current_retour["ok"])
+                    if len(current_retour["ok"]) > 0 :
+                        stack_urls.append(current_retour["ok"])
                     sent_urls.append(new_url)
     return retour
 
 def directory(args) :
-    print("\n")
     print(visual.title("START DIRECTORY FUZZING"))
     if args.wordlist is not None :
         if not args.silent :
@@ -125,23 +144,46 @@ def directory(args) :
         retour = url_sent["retour"]
         sent_urls = retour["sent"]
     else :
+        retour = {}
+        retour["url"] = {}
+        retour["intelligent"] = {}
+        retour["title"] = "Directory Fuzzing"
         sent_urls = []
-        words = None
-        retour["ok"] = None
+        words = []
+        retour["ok"] = {}
     if args.intelligent :
-        retour["intelligent"] = intelligent(args, retour["ok"], sent_urls, words)
+        retour["intelligent"] = intelligent(args, sent_urls, words)
         retour["ok"].update(retour["intelligent"]["ok"])
         retour["url"].update(retour["intelligent"]["url"])
     print(visual.title("END DIRECTORY FUZZING"))
     return retour
-        
+
+def process_retour(retour) :
+    for element in retour :
+        if element == "ok" :
+            for url in retour["ok"] :
+                current_response = retour["ok"][url]
+                response = {}
+                response["code"] = current_response.status_code
+                response["text"] = current_response.text
+
+def write_output(args, retour) :
+    if args.output is not None :
+        try :
+            with open(args.output, "w") as file :
+                file.write(json.dumps(retour))
+        except Exception as e:
+            print(visual.error(str(e)))
+            sys.exit()
 
 def main() :
     args = arg_parser.get_args()
     retour = directory(args)
     if not args.silent :
         print("\n")
-    print_retour_directory(retour)
+    print_retour_directory(args, retour)
+    process_retour(retour)
+    write_output(args, retour)
 
 if __name__ == '__main__' : 
     main()
